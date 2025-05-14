4eda799f0ee3a30f2ab24d922e9033ddda224c07
lubna-kudsi <161640337+lubna-kudsi@users.noreply.github.com>
2025-05-14 21:47:55 +0300

    config cvhnages

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,20 @@
 // tailwind.config.js
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: ['./src/*/.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
     extend: {
       width: {
-        '64': '16rem', // this defines the `w-64` width
+        '64': '16rem', // this defines the w-64 width
       },
       colors: {
-        primary: '#0a272b', // Add custom color
+        primary: '#0a272b', 
+        navyy:'#41ab5d',
+        midGreen:'#238b45',
+        purple:'#005a32',
+        dk:'#f7fcf5',
+        dkk:'#e5f5e0',
+        jana:'#c7e9c0',
+        lubna:'#74c476'// Add custom color
       },
         fontFamily: {
           poppins: ['"Poppins"', 'sans-serif'], // Add Poppins font
